e3f25d752c6e69d0f0f07f08d6a594bbac56d5d3
Barun Saha <barun.saha04@gmail.com>
2015-03-19 21:46:26 +0530

    Ubuntu package installation: added install --fix-missing option

--- scripts/install_ubuntu_packages.sh
+++ scripts/install_ubuntu_packages.sh
@@ -21,12 +21,19 @@ log 'Installing necessary Ubuntu packages'
 
 sudo -E apt-get -y install python python-dev python-setuptools python-pip
 sudo -E apt-get -y install build-essential
+# A defensive step
+sudo -E apt-get -y install --fix-missing
 sudo -E apt-get -y install graphviz graphviz-dev
+# A defensive step
+sudo -E apt-get -y install --fix-missing
 sudo -E apt-get -y install libmysqlclient18 libmysqlclient-dev
+# A defensive step
+sudo -E apt-get -y install --fix-missing
 sudo -E apt-get -y install pkg-config
 sudo -E apt-get -y install debconf-utils
 sudo -E apt-get -y install libapache2-mod-wsgi
+# A defensive step
+sudo -E apt-get -y install --fix-missing
 sudo -E apt-get -y install gcc
-
 # A defensive step
 sudo -E apt-get -y install --fix-missing
