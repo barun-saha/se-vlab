fca04d906cf0f21dea2bb4c2f7738a3c081ebf51
Barun Saha <barun.saha04@gmail.com>
2015-03-19 20:04:36 +0530

    Refined apt-get repos; no removal of already existing packages

--- scripts/install_ubuntu_packages.sh
+++ scripts/install_ubuntu_packages.sh
@@ -14,12 +14,16 @@ source ../scripts/common.sh
 
 log 'Installing necessary Ubuntu packages'
 
+#sudo -E apt-get -y remove python python-dev python-setuptools python-pip build-essential graphviz graphviz-dev libmysqlclient18 libmysqlclient-dev pkg-config debconf-utils libapache2-mod-wsgi gcc
+#sudo apt-get clean
+#sudo apt-get autoclean
+#sudo apt-get update
+
 sudo -E apt-get -y install python python-dev python-setuptools python-pip
 sudo -E apt-get -y install build-essential
 sudo -E apt-get -y install graphviz graphviz-dev
 sudo -E apt-get -y install libmysqlclient18 libmysqlclient-dev
 sudo -E apt-get -y install pkg-config
-sudo -E apt-get -y install bash
 sudo -E apt-get -y install debconf-utils
 sudo -E apt-get -y install libapache2-mod-wsgi
 sudo -E apt-get -y install gcc
